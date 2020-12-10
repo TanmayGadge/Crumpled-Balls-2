94ed658b5369903abed620e8818b285d63fc5107
TanmayGadge <tanmaygadge9273@gmail.com>
2020-12-10 12:23:08 +0530

    crumpled balls 2

---
+++ box.js
@@ -0,0 +1,68 @@
+class Box {
+  constructor(x, y) {
+
+    this.BboxImg = loadImage("Images/dustbin.png");
+
+
+var options = {
+      'restitution': 0.8,
+      'friction': 1.0,
+      'density': 1.5,
+      'isStatic': true
+    }
+    this.angle = 0;
+    push();
+    this.Bbody = Bodies.rectangle(x, y+70, 200, 20, options);
+    this.x = x;
+    this.y = y;
+    this.width = width/10;
+    this.height = height/20 ;
+    pop();
+
+    this.Rbody = Bodies.rectangle(x+100, y+100, 20, 100, options);
+    
+    this.Lbody = Bodies.rectangle(x-100, y+100, 20, 100, options);
+    Body.setAngle(this.Lbody, this.angle);
+    Body.setAngle(this.Rbody, (-1) * this.angle);
+
+
+    World.add(world, this.Bbody);
+    World.add(world, this.Lbody);
+    World.add(world, this.Rbody);
+  }
+  display() {
+    var Bpos = this.Bbody.position;
+    var Lpos = this.Lbody.position;
+    var Rpos = this.Rbody.position;
+
+   /* //var angle = this.body.angle;
+    push();
+    translate(Lpos.x, Lpos.y);
+    rotate(this.angle);
+    rectMode(CENTER);
+    fill("black");
+    //rect(Lpos.x, (-1)*Lpos.y, 20, 200);
+    pop();
+
+    push();
+    translate(Rpos.x, Rpos.y);
+    rotate((-1) * this.angle);
+    rectMode(CENTER);
+    fill("black");
+    //rect(0, -120, 20, 200);
+    pop();
+
+    console.log("Lpos.x: "+Lpos.x);
+    console.log("Lpos.y: "+Lpos.y);*/
+
+    push();
+    //translate(Bpos.x, Bpos.y);
+    rotate((-1)*this.angle);
+    imageMode(CENTER);
+    //rectMode(CENTER);
+    fill("black");
+    image(this.BboxImg, this.x, this.y-30, this.width, this.height+200)
+    //rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
---
+++ sketch.js
@@ -0,0 +1,59 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground, paper;
+var box, boxImg;
+var world, engine;
+var b1, b2;
+
+function preload() {
+	boxImg = loadImage("Images/dustbin.png");
+}
+
+function setup() {
+	createCanvas(1800, 400);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+
+
+	//Create the Bodies Here.
+	ground = new Ground(900, 380, 1800, 20);
+
+	box = new Box(1300, 290);
+	
+	
+	
+	paper = new Paper(200, 220, 70, 70);
+	b1 = new Ground(1220, 280, 20, 180);
+	b2 = new Ground(1380, 280, 20, 180);
+
+	
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+	background("grey");
+	
+
+	paper.display();
+	ground.display();
+	box.display();
+
+
+	
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		Body.applyForce(paper.body, paper.body.position, { x: 1130, y: -1130 });
+	}
+}
+
